1be5708a07284279a053c3f3294fb023314e0fc2
Mathis Salmen <mathis.salmen@matsal.de>
2024-02-08 13:55:17 +0100

    output optimized llvm ir

--- llvm/tools/pattern-gen/LLVMOverride.hpp
+++ llvm/tools/pattern-gen/LLVMOverride.hpp
@@ -2,4 +2,4 @@
 #include "llvm/IR/Module.h"
 #include "llvm/Support/CodeGen.h"
 
-int RunPatternGenPipeline(llvm::Module* M, std::string mattr, llvm::CodeGenOptLevel optLevel);
+int RunPatternGenPipeline(llvm::Module* M, std::string mattr, llvm::CodeGenOptLevel optLevel, std::ostream &irOut);
