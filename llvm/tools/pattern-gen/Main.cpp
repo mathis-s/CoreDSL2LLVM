537228528e77b38064d15f453229101baf3d3f96
Philipp van Kempen <philipp.van-kempen@tum.de>
2024-03-19 08:04:24 +0100

    Main.cpp: refactor get_out_streams

--- llvm/tools/pattern-gen/Main.cpp
+++ llvm/tools/pattern-gen/Main.cpp
@@ -72,7 +72,7 @@ static cl::opt<std::string> Predicates(
 #include <iostream>
 namespace fs = std::filesystem;
 
-static auto get_out_streams(std::string srcPath, std::string destPath) {
+static auto get_out_streams(std::string srcPath, std::string destPath, bool emitLL) {
   fs::path outPath{destPath};
 
   fs::path inPath{srcPath};
@@ -84,9 +84,18 @@ static auto get_out_streams(std::string srcPath, std::string destPath) {
     newExt = outPath.extension();
   }
   // TODO: allow .td in out path
-  std::string irPath = basePath.string() + ".ll";
-  std::string fmtPath = basePath.string() + "InstrFormat" + newExt;
-  std::string patPath = basePath.string() + newExt;
+  std::string irPath = "/dev/null";
+  std::string fmtPath = "/dev/null";
+  std::string patPath = "/dev/null";
+  if (emitLL) {
+    irPath = basePath.string() + ".ll";
+  }
+  if (!SkipFmt) {
+    fmtPath = basePath.string() + "InstrFormat" + newExt;
+  }
+  if (!SkipPat) {
+    patPath = basePath.string() + newExt;
+  }
 
   return std::make_tuple(std::ofstream(irPath), std::ofstream(fmtPath),
                          std::ofstream(patPath));
