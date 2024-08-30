113992c4e49b45f36c172bec99c006462727b603
Philipp van Kempen <philipp.van-kempen@tum.de>
2024-08-30 08:16:08 +0200

    PatternGen: Run createRISCVO0PreLegalizerCombiner in LLVMOverride.cpp

--- llvm/tools/pattern-gen/LLVMOverride.cpp
+++ llvm/tools/pattern-gen/LLVMOverride.cpp
@@ -146,6 +146,7 @@ public:
   bool addPreISel() override;
   bool addInstSelector() override;
   bool addIRTranslator() override;
+  void addPreLegalizeMachineIR() override;
   bool addLegalizeMachineIR() override;
   bool addRegBankSelect() override;
   bool addGlobalInstructionSelect() override;
@@ -208,6 +209,13 @@ bool RISCVPatternPassConfig::addInstSelector() {
 bool RISCVPatternPassConfig::addIRTranslator() {
   addPass(new IRTranslator(getOptLevel()));
   return false;
+}
+
+void RISCVPatternPassConfig::addPreLegalizeMachineIR() {
+  if (getOptLevel() == CodeGenOptLevel::None) {
+    addPass(createRISCVO0PreLegalizerCombiner());
+  } else {                                                                                                             addPass(createRISCVPreLegalizerCombiner());
+  }
 }
 
 bool RISCVPatternPassConfig::addLegalizeMachineIR() {
