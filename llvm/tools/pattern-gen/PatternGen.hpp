e9d39bf1475d2c91f8d6cbc6aa86b3297be900f9
Philipp van Kempen <philipp.van-kempen@tum.de>
2024-09-29 07:44:21 +0200

    [lint] address clang-tidy issues in llvm/tools/pattern-gen/PatternGen.{cpp,hpp}

--- llvm/tools/pattern-gen/PatternGen.hpp
+++ llvm/tools/pattern-gen/PatternGen.hpp
@@ -9,9 +9,9 @@ struct PGArgsStruct
     std::string Mattr;
     llvm::CodeGenOptLevel OptLevel;
     std::string Predicates;
-    bool is64Bit;
+    bool Is64Bit;
 };
 
-int OptimizeBehavior(llvm::Module* M, std::vector<CDSLInstr> const& instrs, std::ostream& ostreamIR, PGArgsStruct args);
-int GeneratePatterns(llvm::Module* M, std::vector<CDSLInstr> const& instrs, std::ostream& ostream, PGArgsStruct args);
+int optimizeBehavior(llvm::Module* M, std::vector<CDSLInstr> const& Instrs, std::ostream& OstreamIR, PGArgsStruct Args);
+int generatePatterns(llvm::Module* M, std::vector<CDSLInstr> const& Instrs, std::ostream& Ostream, PGArgsStruct Args);
 //void PrintPattern(llvm::SelectionDAG& DAG);
